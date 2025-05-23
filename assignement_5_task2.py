0c345e898b3396ae1b86ce2c2299369cad6d0a80
smishr31 <sajalmishra910@gmail.com>
2025-05-24 00:33:44 +0530

    Add files via upload

---
+++ assignement_5_task2.py
@@ -0,0 +1,7 @@
+
+numbers = list(range(1, 11))
+print(numbers)
+first_five = numbers[:5]
+reversed_five = first_five[::-1]
+print("First five elements:", first_five)
+print("Reversed list:", reversed_five)
